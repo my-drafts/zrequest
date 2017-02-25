632b3210e792448a18692ca1d2e93a0ad7f2a8e5
Sergej Kukharev <sergej.kucharev@gmail.com>
2017-02-25 10:51:10 +0200

    2.0.0

--- test/index.js
+++ test/index.js
@@ -3,10 +3,40 @@
 'use strict';
 
 const http = require('http');
-const ZRequest = require('../');
+const ZRequest = require('../').ZRequest;
 const zt = require('ztype');
 
 http.createServer(function (request, response) {
 	let req = new ZRequest(request);
-	console.log(String(req));
-}).listen(8080);
+	//console.log(String(req));
+	if (req.path === '/f/') {
+		const html = '\
+<html>\
+<body>\
+<form method="POST" action="/post" enctype="multipart/form-data">\
+<input type="text" name="aa" value="" />\
+<input type="text" name="aa" value="" />\
+<input type="text" name="bb" value="" />\
+<input type="file" name="ff" value="" />\
+<input type="submit" value="submit" />\
+<input type="reset" value="reset" />\
+</form>\
+</body>\
+</html>\
+		';
+		response.end(html);
+	}
+	else {
+		req.init().then(function () {
+			return req.uploadClean();
+		}).then(function () {
+			console.log(String(req));
+			response.end(String(req));
+		}).catch(function (error) {
+			console.log(error);
+			response.end(String(error));
+		});
+	}
+}).on('error', function (error) {
+	console.log(error);
+}).listen(8081);
