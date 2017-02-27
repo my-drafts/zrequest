c8e0d82b72ae7a12899b6c4b45ab9aa393b35738
Sergyi Kukharyev <sergej.kucharev@gmail.com>
2017-02-27 21:26:55 +0200

    test fix

--- test/index.js
+++ test/index.js
@@ -13,29 +13,35 @@ const HTML = '<html><head>%s</head><body>%s</body></html>';
 
 http.createServer(function (request, response) {
 	let req = new ZRequest(request);
-	//console.log(String(req));
 	if (req.path === '/') {
 		const head = '<title>test frontpage</title>';
 		const body = '\
 <ul>\
 <li><a href="/">frontpage</a><li>\
-<li><a href="/multipart-form-data/">multipart/form-data</a><li>\
-<li><a href="/application-json/">application/json</a><li>\
-<li><a href="/application-www-urlencoded-form-data/">application/www-urlencoded-form-data</a><li>\
-<li><a href="/text-plain/">text/plain</a><li>\
+<li><a href="/post-multipart-form-data/">POST: multipart/form-data</a><li>\
+<li><a href="/post-application-json/">POST: application/json</a><li>\
+<li><a href="/post-application-x-www-form-urlencoded/">POST: application/x-www-form-urlencoded</a><li>\
+<li><a href="/post-text-plain/">POST: text/plain</a><li>\
+<li><a href="/post/">POST</a><li>\
+<li><a href="/get/">GET</a><li>\
+<li><a href="/submit-simple/">submit-simple</a><li>\
+<li><a href="/submit-complex/">submit-complex</a><li>\
 </ul>\
 		';
+		console.log(String(req));
+		console.log('---');
 		response.end(uf(HTML, head, body));
 	}
 	else if (req.path === '/submit-simple/'){
 		console.log(String(req));
+		console.log('---');
 		response.end(String(req));
 	}
 	else if (req.path === '/submit-complex/'){
 		const options = require('./config');
 		req.init(options).then(function () {
 			return new Promise(function (resolve, reject) {
-				const uploadDirPath = __dirname.replace(/[\/][^\/]+$/, '') + options.postAndUploadInit.directory;
+				const uploadDirPath = __dirname.replace(/[^\/]+$/, '') + options.postAndUploadInit.directory;
 				fs.readdir(uploadDirPath, function (error, files) {
 					error ? reject(error) : resolve(files);
 				});
@@ -45,13 +51,15 @@ http.createServer(function (request, response) {
 			return req.final(options);
 		}).then(function () {
 			console.log(String(req));
+			console.log('---');
 			response.end(String(req));
 		}).catch(function (error) {
 			console.log(error);
+			console.log('---');
 			response.end(String(error));
 		});
 	}
-	else if (req.path === '/multipart-form-data/') {
+	else if (req.path === '/post-multipart-form-data/') {
 		const head = '<title>post: multipart/form-data</title>';
 		const body = '\
 <form method="POST" action="/submit-complex/" enctype="multipart/form-data">\
@@ -63,12 +71,14 @@ http.createServer(function (request, response) {
 <input type="reset" value="reset" />\
 </form>\
 		';
+		console.log(String(req));
+		console.log('---');
 		response.end(uf(HTML, head, body));
 	}
-	else if (req.path === '/application-json/') {
+	else if (req.path === '/post-application-json/') {
 		const head = '<title>post: application/json</title>';
 		const body = '\
-<form method="POST" action="/post" enctype="application/json">\
+<form method="POST" action="/submit-complex/" enctype="application/json">\
 <input type="text" name="aa" value="" />\
 <input type="text" name="aa" value="" />\
 <input type="text" name="bb" value="" />\
@@ -77,13 +87,80 @@ http.createServer(function (request, response) {
 <input type="reset" value="reset" />\
 </form>\
 		';
+		console.log(String(req));
+		console.log('---');
+		response.end(uf(HTML, head, body));
+	}
+	else if (req.path === '/post-application-x-www-form-urlencoded/') {
+		const head = '<title>post: application/x-www-form-urlencoded</title>';
+		const body = '\
+<form method="POST" action="/submit-complex/" enctype="application/x-www-form-urlencoded">\
+<input type="text" name="aa" value="" />\
+<input type="text" name="aa" value="" />\
+<input type="text" name="bb" value="" />\
+<input type="file" multiple="multiple" name="ff" value="" />\
+<input type="submit" value="submit" />\
+<input type="reset" value="reset" />\
+</form>\
+		';
+		console.log(String(req));
+		console.log('---');
+		response.end(uf(HTML, head, body));
+	}
+	else if (req.path === '/post-text-plain/') {
+		const head = '<title>post: text/plain</title>';
+		const body = '\
+<form method="POST" action="/submit-complex/" enctype="text/plain">\
+<input type="text" name="aa" value="" />\
+<input type="text" name="aa" value="" />\
+<input type="text" name="bb" value="" />\
+<input type="file" multiple="multiple" name="ff" value="" />\
+<input type="submit" value="submit" />\
+<input type="reset" value="reset" />\
+</form>\
+		';
+		console.log(String(req));
+		console.log('---');
+		response.end(uf(HTML, head, body));
+	}
+	else if (req.path === '/post/') {
+		const head = '<title>post</title>';
+		const body = '\
+<form method="POST" action="/submit-complex/">\
+<input type="text" name="aa" value="" />\
+<input type="text" name="aa" value="" />\
+<input type="text" name="bb" value="" />\
+<input type="file" multiple="multiple" name="ff" value="" />\
+<input type="submit" value="submit" />\
+<input type="reset" value="reset" />\
+</form>\
+		';
+		console.log(String(req));
+		console.log('---');
+		response.end(uf(HTML, head, body));
+	}
+	else if (req.path === '/get/') {
+		const head = '<title>get</title>';
+		const body = '\
+<form method="GET" action="/submit-simple/">\
+<input type="text" name="aa" value="" />\
+<input type="text" name="aa" value="" />\
+<input type="text" name="bb" value="" />\
+<input type="file" multiple="multiple" name="ff" value="" />\
+<input type="submit" value="submit" />\
+<input type="reset" value="reset" />\
+</form>\
+		';
+		console.log(String(req));
+		console.log('---');
 		response.end(uf(HTML, head, body));
 	}
 	else {
-		console.log('else:');
 		console.log(String(req));
+		console.log('---');
 		response.end(String(req));
 	}
 }).on('error', function (error) {
 	console.log(error);
-}).listen(8082);
+	console.log('---');
+}).listen(8080);
