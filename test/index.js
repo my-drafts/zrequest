f0db0591830ffa07d64e0193100773ef1a1ab400
Sergej <sergej.kucharev@gmail.com>
2017-02-27 12:45:49 +0200

    test fix

--- test/index.js
+++ test/index.js
@@ -21,6 +21,8 @@ http.createServer(function (request, response) {
 <li><a href="/">frontpage</a><li>\
 <li><a href="/multipart-form-data/">multipart/form-data</a><li>\
 <li><a href="/application-json/">application/json</a><li>\
+<li><a href="/application-www-urlencoded-form-data/">application/www-urlencoded-form-data</a><li>\
+<li><a href="/text-plain/">text/plain</a><li>\
 </ul>\
 		';
 		response.end(uf(HTML, head, body));
@@ -52,7 +54,7 @@ http.createServer(function (request, response) {
 	else if (req.path === '/multipart-form-data/') {
 		const head = '<title>post: multipart/form-data</title>';
 		const body = '\
-<form method="POST" action="/post" enctype="multipart/form-data">\
+<form method="POST" action="/submit-complex/" enctype="multipart/form-data">\
 <input type="text" name="aa" value="" />\
 <input type="text" name="aa" value="" />\
 <input type="text" name="bb" value="" />\
@@ -66,42 +68,21 @@ http.createServer(function (request, response) {
 	else if (req.path === '/application-json/') {
 		const head = '<title>post: application/json</title>';
 		const body = '\
-<html>\
-<body>\
 <form method="POST" action="/post" enctype="application/json">\
 <input type="text" name="aa" value="" />\
 <input type="text" name="aa" value="" />\
 <input type="text" name="bb" value="" />\
-<input type="file" name="ff" value="" />\
+<input type="file" multiple="multiple" name="ff" value="" />\
 <input type="submit" value="submit" />\
 <input type="reset" value="reset" />\
 </form>\
-</body>\
-</html>\
 		';
 		response.end(uf(HTML, head, body));
 	}
-	else if (req.path === '/post/'){
-		let options = {};
-		req.init(options).then(function () {
-			return new Promise(function (resolve, reject) {
-				const uploadDirPath = path.normalize(__dirname + '/../tmp/upload');
-				fs.readdir(uploadDirPath, function (error, files) {
-					error ? reject(error) : resolve(files);
-				});
-			});
-		}).then(function (files) {
-			console.log(files);
-			return Promise.resolve();
-		}).then(function () {
-			return req.final(options);
-		}).then(function () {
-			console.log(String(req));
-			response.end(String(req));
-		}).catch(function (error) {
-			console.log(error);
-			response.end(String(error));
-		});
+	else {
+		console.log('else:');
+		console.log(String(req));
+		response.end(String(req));
 	}
 }).on('error', function (error) {
 	console.log(error);
